1b9b084d6265e2c460ce1cc26d8ee45d17aa4162
AsterSea <858256500@qq.com>
2019-05-04 21:54:12 +0800

    success generate

--- src/main/java/neu/lab/conflict/writer/SemanticsConflictWriter.java
+++ src/main/java/neu/lab/conflict/writer/SemanticsConflictWriter.java
@@ -11,12 +11,14 @@ import java.util.Set;
 
 import org.evosuite.EvoSuite;
 import org.evosuite.Properties;
+import org.evosuite.TestSuiteGenerator;
 import org.evosuite.ga.Chromosome;
 import org.evosuite.ga.metaheuristics.GeneticAlgorithm;
 import org.evosuite.result.TestGenerationResult;
 import org.evosuite.testsuite.TestSuiteChromosome;
 
 import neu.lab.conflict.SemanticsConflictMojo;
+import shaded.org.evosuite.classpath.ClassPathHandler;
 
 public class SemanticsConflictWriter {
 	public void writeSemanticsConflict(String outPath) {
@@ -24,6 +26,7 @@ public class SemanticsConflictWriter {
 		try {
 			printer = new PrintWriter(new BufferedWriter(new FileWriter(outPath + "SemeanticsConflict.txt", true)));
 //			write(printer);
+			write();
 			printer.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
@@ -32,22 +35,32 @@ public class SemanticsConflictWriter {
 	}// PrintWriter printer
 
 	private void write() {
-		EvoSuite evosuite = new EvoSuite();
-		String targetClass = SemanticsConflictWriter.class.getCanonicalName();
+		TestSuiteGenerator testSuiteGenerator = new TestSuiteGenerator();
+//		EvoSuite evosuite = new EvoSuite();
+		String targetClass = "org.bytedeco.javacpp.BooleanPointer";// SemanticsConflictWriter.class.getCanonicalName();
+		Properties.getInstance();
+		Properties.CP = "C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\target\\classes\\;C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\target\\dependency;C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\evosuite-master-1.0.6.jar;C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\evosuite-runtime-1.0.6.jar";
+		ClassPathHandler.getInstance().setEvoSuiteClassPath(
+				new String[] { "C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\evosuite-master-1.0.6.jar",
+						"C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\evosuite-runtime-1.0.6.jar" });
+		Properties.REGRESSIONCP = "C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\target\\classes\\";
 		Properties.TARGET_CLASS = targetClass;
-		String[] paths = readFiles("target/dependency/").toArray(new String[] {});
-		String[] command = new String[] { "-generateSuite", "-class", targetClass };
-		Set<String> test = new HashSet<String>();
-		test.add("-setup");
-		test.add("target/classes");
-		for (String path:paths) {
-			test.add(path.replace('\\', '/'));
-		}
-		evosuite.parseCommandLine(test.toArray(new String[] {}));
-		Object result = evosuite.parseCommandLine(command);
-		TestGenerationResult ga = getGAFromResult(result);
-//		TestSuiteChromosome test0 = (TestSuiteChromosome) ga.getBestIndividual();
-		System.out.println(ga.getGeneticAlgorithm());
+		Properties.CLASSPATH = new String[] { "C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\target\\classes",
+				"C:\\Users\\Flipped\\Desktop\\project\\javacpp-1.4.4\\target\\dependency" };
+		TestGenerationResult result = testSuiteGenerator.generateTestSuite();
+//		String[] paths = readFiles(".\\target\\dependency\\").toArray(new String[] {});
+//		String[] command = new String[] { "-generateSuite", "-class", targetClass };
+//		Set<String> test = new HashSet<String>();
+//		test.add("-setup");
+//		test.add(".\\target\\classes");
+//		for (String path:paths) {
+//			test.add(path.replace('\\', '/'));
+//		}
+//		evosuite.parseCommandLine(test.toArray(new String[] {}));
+//		Object result = evosuite.parseCommandLine(command);
+//		TestGenerationResult ga = getGAFromResult(result);
+//		TestSuiteChromosome test0 =  (TestSuiteChromosome) ga.getBestIndividual();
+		System.out.println(result);
 	}
 
 	public static void main(String[] args) {
