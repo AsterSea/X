0320273d53f06a45a4178ffa69a1926f977bef02
AsterSea <858256500@qq.com>
2019-05-06 00:46:23 +0800

    2019.5.6

--- src/main/java/neu/lab/evosuiteshell/Config.java
+++ src/main/java/neu/lab/evosuiteshell/Config.java
@@ -1,5 +1,36 @@
 package neu.lab.evosuiteshell;
 
+import java.io.File;
+
+import neu.lab.conflict.util.MavenUtil;
+
 public class Config {
-public static String MAVEN_PATH="mvn.bat";
+	public static String MAVEN_PATH = "";
+
+	/**
+	 * get mvn.bat or mvn.cmd path
+	 * @return
+	 */
+	public static String getMaven() {
+		if (!MAVEN_PATH.equals("")) {
+			return MAVEN_PATH;
+		}
+		String properties = System.getProperty("java.library.path");
+		String mavenPath = "";
+		String[] paths = properties.split(";");
+		for (String path : paths) {
+			if (path.contains("maven")) {
+				mavenPath = path;
+				break;
+			}
+		}
+		if (new File(mavenPath + "\\mvn.bat").exists())
+			MAVEN_PATH = mavenPath + "\\mvn.bat";
+		else if (new File(mavenPath + "\\mvn.cmd").exists())
+			MAVEN_PATH = mavenPath + "\\mvn.cmd";
+		if (MAVEN_PATH.equals("")) {
+			MavenUtil.i().getLog().error("Please check or set maven home!");
+		}
+		return MAVEN_PATH;
+	}
 }
