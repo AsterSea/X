e19df3b094c6fd723514309889bf6503ca1cd5db
wangcwangc <858256500@qq.com>
2020-02-06 02:53:27 +0800

    add finally

--- src/main/java/neu/lab/conflict/writer/ClassDupRiskWriter.java
+++ src/main/java/neu/lab/conflict/writer/ClassDupRiskWriter.java
@@ -30,71 +30,75 @@ import neu.lab.conflict.vo.DupClsJarPair;
 
 public class ClassDupRiskWriter {
 
-	private ClassDups classDups;
+    private ClassDups classDups;
 
-	private DupClsJarPairs jarPairs;
+    private DupClsJarPairs jarPairs;
 
-	public ClassDupRiskWriter() {
-		classDups = new ClassDups(DepJars.i());
-	}
+    public ClassDupRiskWriter() {
+        classDups = new ClassDups(DepJars.i());
+    }
 
-	public void writeByClass() {
-		try {
-			PrintWriter printer = new PrintWriter(
-					new BufferedWriter(new FileWriter(new File(Conf.outDir + "classDupRisk.txt"), true)));
-			printer.println("projectPath->" + MavenUtil.i().getProjectInfo());
+    public void writeByClass() {
+        PrintWriter printer = null;
+        try {
+            printer = new PrintWriter(
+                    new BufferedWriter(new FileWriter(new File(Conf.outDir + "classDupRisk.txt"), true)));
+            printer.println("projectPath->" + MavenUtil.i().getProjectInfo());
 
-			printer.println("class duplicate:");
-			for (ClassDup classDup : classDups.getAllClsDup()) {
-				printer.println(classDup.getRiskString());
-			}
-			printer.println("\n\n\n\n");
-			printer.close();
-		} catch (Exception e) {
-			MavenUtil.i().getLog().error("can't write classDupByClass:", e);
-		}
-	}
+            printer.println("class duplicate:");
+            for (ClassDup classDup : classDups.getAllClsDup()) {
+                printer.println(classDup.getRiskString());
+            }
+            printer.println("\n\n\n\n");
+        } catch (Exception e) {
+            MavenUtil.i().getLog().error("can't write classDupByClass:", e);
+        } finally {
+            printer.close();
+        }
+    }
 
-	public void writeByJarNoClassMissing(String outPath) {
-		try {
-			String fileName = MavenUtil.i().getProjectGroupId() + "_" + MavenUtil.i().getProjectArtifactId() + "_"
-					+ MavenUtil.i().getProjectVersion();
-			PrintWriter printer = new PrintWriter(new BufferedWriter(
-					new FileWriter(new File(outPath + fileName.replace('.', '_') + "DupByJar.txt"), true)));
+    public void writeByJarNoClassMissing(String outPath) {
+        PrintWriter printer = null;
+        try {
+            String fileName = MavenUtil.i().getProjectGroupId() + "_" + MavenUtil.i().getProjectArtifactId() + "_"
+                    + MavenUtil.i().getProjectVersion();
+            printer = new PrintWriter(new BufferedWriter(
+                    new FileWriter(new File(outPath + fileName.replace('.', '_') + "DupByJar.txt"), true)));
 
-			for (DupClsJarPair jarPair : getJarPairs().getAllJarPair()) {
+            for (DupClsJarPair jarPair : getJarPairs().getAllJarPair()) {
 //				printer.println("projectPath->" + MavenUtil.i().getProjectInfo());
-				write(jarPair, printer);
+                write(jarPair, printer);
 //				printer.println(jarPair.getJar1().getJarFilePaths(false));
-			}
-			printer.close();
-		} catch (Exception e) {
-			MavenUtil.i().getLog().error("can't write classDupByJar:", e);
-		}
-	}
+            }
+        } catch (Exception e) {
+            MavenUtil.i().getLog().error("can't write classDupByJar:", e);
+        } finally {
+            printer.close();
+        }
+    }
 
-	public void write(DupClsJarPair jarPair, PrintWriter printer) {
+    public void write(DupClsJarPair jarPair, PrintWriter printer) {
 //		printer.println(jarPair.getCommonMethods());
-		Graph4path pathGraph = jarPair.getMethoPathGraphForSemanteme();
-		Set<String> hostNodes = pathGraph.getHostNodes();
+        Graph4path pathGraph = jarPair.getMethoPathGraphForSemanteme();
+        Set<String> hostNodes = pathGraph.getHostNodes();
         Map<String, String> methodMappingASMMethod = pathGraph.getMethodMappingASMMethod();
         Map<String, List<Operation>> allRiskMethodDiffsMap = jarPair.getRiskMethodDiffsMap();
 //		System.out.println(hostNodes.toString());
-		Map<String, IBook> pathBooks = new Dog(pathGraph).findRlt(hostNodes, Conf.DOG_DEP_FOR_PATH,
-				Strategy.NOT_RESET_BOOK);
-		MySortedMap<Integer, Record4path> dis2records = new MySortedMap<Integer, Record4path>();
-		for (String topMthd : pathBooks.keySet()) {
-			if (hostNodes.contains(topMthd)) {
-				Book4path book = (Book4path) (pathBooks.get(topMthd));
-				for (IRecord iRecord : book.getRecords()) {
-					Record4path record = (Record4path) iRecord;
-					dis2records.add(record.getPathlen(), record);
-				}
-			}
-		}
+        Map<String, IBook> pathBooks = new Dog(pathGraph).findRlt(hostNodes, Conf.DOG_DEP_FOR_PATH,
+                Strategy.NOT_RESET_BOOK);
+        MySortedMap<Integer, Record4path> dis2records = new MySortedMap<Integer, Record4path>();
+        for (String topMthd : pathBooks.keySet()) {
+            if (hostNodes.contains(topMthd)) {
+                Book4path book = (Book4path) (pathBooks.get(topMthd));
+                for (IRecord iRecord : book.getRecords()) {
+                    Record4path record = (Record4path) iRecord;
+                    dis2records.add(record.getPathlen(), record);
+                }
+            }
+        }
 //		Map<String, List<Integer>> semantemeMethodForDifferences = jarPair.getSemantemeMethodForDifferences();
         int num = 0;
-		if (dis2records.size() > 0) {
+        if (dis2records.size() > 0) {
             Map<String, Integer> sortMap = new TreeMap<>();
             Set<String> hasWriterRiskMethodPath = new HashSet<>();
             printer.println("conflict:" + jarPair.toString());
@@ -134,35 +138,35 @@ public class ClassDupRiskWriter {
                 }
             }
         }
-	}
+    }
 
-//
-	public void writeByJarHasClassMissing(String outPath) {
-		try {
-			String fileName = MavenUtil.i().getProjectGroupId() + "_" + MavenUtil.i().getProjectArtifactId() + "_"
-					+ MavenUtil.i().getProjectVersion();
-			PrintWriter printer = new PrintWriter(new BufferedWriter(
-					new FileWriter(new File(outPath + fileName.replace('.', '_') + "DupByJar.txt"), true)));
+    //
+    public void writeByJarHasClassMissing(String outPath) {
+        try {
+            String fileName = MavenUtil.i().getProjectGroupId() + "_" + MavenUtil.i().getProjectArtifactId() + "_"
+                    + MavenUtil.i().getProjectVersion();
+            PrintWriter printer = new PrintWriter(new BufferedWriter(
+                    new FileWriter(new File(outPath + fileName.replace('.', '_') + "DupByJar.txt"), true)));
 
-			for (Conflict conflict : Conflicts.i().getConflicts()) {
-				printer.println("projectPath->" + MavenUtil.i().getProjectInfo());
-				for (DepJarJRisk depJarJRisk : conflict.getJarRisks()) {
+            for (Conflict conflict : Conflicts.i().getConflicts()) {
+                printer.println("projectPath->" + MavenUtil.i().getProjectInfo());
+                for (DepJarJRisk depJarJRisk : conflict.getJarRisks()) {
 //					System.out.println(depJarJRisk.toString());
-					ClassDups newClassDups = new ClassDups(DepJars.i(), depJarJRisk);
+                    ClassDups newClassDups = new ClassDups(DepJars.i(), depJarJRisk);
 //					System.out.println(newClassDups.getAllClsDup().size());
-					DupClsJarPairs newJarPairs = new DupClsJarPairs(newClassDups);
+                    DupClsJarPairs newJarPairs = new DupClsJarPairs(newClassDups);
 //					System.out.println(newJarPairs.getAllJarPair().size());
-					for (DupClsJarPair jarPair : newJarPairs.getAllJarPair()) {
+                    for (DupClsJarPair jarPair : newJarPairs.getAllJarPair()) {
 //						printer.println(jarPair.toString());
-						write(jarPair, printer);
-					}
-				}
-			}
-			printer.close();
-		} catch (Exception e) {
-			MavenUtil.i().getLog().error("can't write classDupByJar:", e);
-		}
-	}
+                        write(jarPair, printer);
+                    }
+                }
+            }
+            printer.close();
+        } catch (Exception e) {
+            MavenUtil.i().getLog().error("can't write classDupByJar:", e);
+        }
+    }
 //	public void writeRchNum(String outPath, boolean append) {
 //		try {
 //			CSVPrinter printer = new CSVPrinter(new FileWriter(outPath, append), CSVFormat.DEFAULT);
@@ -180,26 +184,26 @@ public class ClassDupRiskWriter {
 //		}
 //	}
 
-	private DupClsJarPairs getJarPairs() {
-		if (jarPairs == null)
-			jarPairs = new DupClsJarPairs(classDups);
-		return jarPairs;
-	}
+    private DupClsJarPairs getJarPairs() {
+        if (jarPairs == null)
+            jarPairs = new DupClsJarPairs(classDups);
+        return jarPairs;
+    }
 
-	private String addJarPath(String mthdCallPath) {
-		StringBuilder sb = new StringBuilder();
-		String[] mthds = mthdCallPath.split("\\n");
-		for (int i = 0; i < mthds.length - 1; i++) {
-			// last method is risk method,don't need calculate.
-			String mthd = mthds[i];
-			String cls = SootUtil.mthdSig2cls(mthd);
-			DepJar depJar = DepJars.i().getClassJar(cls);
-			String jarPath = "";
-			if (depJar != null)
-				jarPath = depJar.getJarFilePaths(true).get(0);
-			sb.append(mthd + " " + jarPath + "\n");
-		}
-		sb.append(mthds[mthds.length - 1]);
-		return sb.toString();
-	}
+    private String addJarPath(String mthdCallPath) {
+        StringBuilder sb = new StringBuilder();
+        String[] mthds = mthdCallPath.split("\\n");
+        for (int i = 0; i < mthds.length - 1; i++) {
+            // last method is risk method,don't need calculate.
+            String mthd = mthds[i];
+            String cls = SootUtil.mthdSig2cls(mthd);
+            DepJar depJar = DepJars.i().getClassJar(cls);
+            String jarPath = "";
+            if (depJar != null)
+                jarPath = depJar.getJarFilePaths(true).get(0);
+            sb.append(mthd + " " + jarPath + "\n");
+        }
+        sb.append(mthds[mthds.length - 1]);
+        return sb.toString();
+    }
 }
